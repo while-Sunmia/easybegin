205d4817114ab8bf00df38be7e98e2019a65a6d2
while-Sunmia <sunmiaprasad@gmail.com>
2026-07-06 19:53:17 +0530

    Update pass_gen.py

--- pass_gen.py
+++ pass_gen.py
@@ -4,4 +4,4 @@ import streamlit as st
 
 st.title("PASSWORD GENERATOR")
 st.text("This is an app that generates a strong password for your internet purposes:")
-limit = int(st.text_input("What is the length of ur alphanumeric password ?"))
+limit = int(st.text_input("What is the length of ur alphanumeric password ???"))
