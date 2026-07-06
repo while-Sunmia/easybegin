811067bc54d55159a963c39988f067762c050562
while-Sunmia <sunmiaprasad@gmail.com>
2026-07-06 20:43:19 +0530

    Refactor password generation to use a list
    Change password storage from string to list and join for output.

--- pass_gen.py
+++ pass_gen.py
@@ -5,10 +5,12 @@ import string
 
 st.title("PASSWORD GENERATOR")
 st.text("This is an app that generates a strong password for your internet purposes:")
-char = 0
+char = []
 limit = int(st.text_input("What is the length of ur alphanumeric password ?"))
 if st.button("Generate!"):
   if i in limit:
     gen = random.choice(string.ascii_letters + string.digits)
-    char=char+gen
-  st.subheader(f"Your generated password is {char}")
+    char.append(gen)
+
+  word = "".join(char)
+  st.subheader(f"Your generated password is {word}")
