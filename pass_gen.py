2c728b2be391375bfa0346254c2f19eed50fcee7
while-Sunmia <sunmiaprasad@gmail.com>
2026-07-06 20:19:45 +0530

    Improve password generation functionality
    Fix password generation logic and add character selection.

--- pass_gen.py
+++ pass_gen.py
@@ -1,9 +1,14 @@
 
 import streamlit as st
 import random
+import string
 
 st.title("PASSWORD GENERATOR")
 st.text("This is an app that generates a strong password for your internet purposes:")
+char = 0
 limit = int(st.text_input("What is the length of ur alphanumeric password ?"))
-gen = random(size = limit)
-st.subheader(f"Your generated password is {gen}")
+if st.button("Generate!"):
+  if i in limit:
+    gen = random.choice(string.ascii_letters + string.digits)
+    char=char+gen
+  st.subheader(f"Your generated password is {char}")
