fbcfabde8f2a0ad6d4520e81665682b553bb7e79
while-Sunmia <sunmiaprasad@gmail.com>
2026-07-06 20:44:05 +0530

    Fix password generation logic in pass_gen.py

--- pass_gen.py
+++ pass_gen.py
@@ -8,7 +8,7 @@ st.text("This is an app that generates a strong password for your internet purpo
 char = []
 limit = int(st.text_input("What is the length of ur alphanumeric password ?"))
 if st.button("Generate!"):
-  if i in limit:
+  if i in range(limit):
     gen = random.choice(string.ascii_letters + string.digits)
     char.append(gen)
 
