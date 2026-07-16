735c4d15b35beb96cb8f0f6e934a1dc6b6023158
Sunmia Prasad <sunmiaprasad@gmail.com>
2026-07-16 20:47:21 +0530

    Add files via upload

---
+++ word_freq.py
@@ -0,0 +1,14 @@
+import string
+import streamlit as st
+st.title("Word frequency App")
+st.write("This is an app that shows you many times a word has appeared and also highlights the specified word in the paragraph")
+pas = st.text_input("Enter your paragraph/text/article: ")
+word = st.text_input("Enter the word: ")
+
+count = 0
+for i in pas.split():
+    wo = i.lower().strip(".,/?!'-")
+    if(wo==word):
+        count = count + 1
+
+print(count)
