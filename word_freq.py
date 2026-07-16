16c05d713a6853a9b193e1485579311e13fcbfbd
Sunmia Prasad <sunmiaprasad@gmail.com>
2026-07-17 00:08:50 +0530

    Update word_freq.py

--- word_freq.py
+++ word_freq.py
@@ -7,8 +7,8 @@ word = st.text_input("Enter the word: ")
 
 count = 0
 if st.button("Show me the frequency:"):
- for i in pas.split():
-   wo = i.lower().strip(".,/?!'-")
-   if(wo==word):
-    count = count + 1
+  for i in pas.split():
+    wo = i.lower().strip(".,/?!'-")
+    if(wo==word):
+     count = count + 1
   st.subheader(f"The word '{word}' has been used {count} times")
